3018ccdfe76f45f0290eb0b2e1bf3ef01968ce92
Joshua Jibilian <jwjibilian@gmail.com>
2016-01-29 21:14:01 -0500

    gradle build

---
+++ src/main/java/test.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Blaze on 1/29/2016.
+ */
+public class test {
+    public static void main(String[] arg) {
+        DBdriver x = new DBdriver();
+        x.sendUpdate("INSERT INTO testing VALUES ('MUAHAHAHA');");
+    }
+
+
+}
